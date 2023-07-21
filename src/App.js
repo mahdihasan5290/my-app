f2391662705d97d57fdba7728e8b3942c65e39f7
Mahdi Hasan <139851599+mahdihasan5290@users.noreply.github.com>
2023-07-21 20:07:22 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import About from './components/About';
+// import About from './components/About';
 import Alert from './components/Alert';
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+// import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState ('light');
@@ -36,16 +36,17 @@ function App() {
   }
   return (
     <>
-<Router>
+{/* <Router> */}
   <Navbar title="Mahdi Hasan" about="About Us" mode={mode} toggleMode={toggleMode} />
   <Alert alert={alert}/>
   <div className="container my-3"> 
-  <Routes>
-          <Route path="/" element={<TextForm showAlert={showAlert} heading= "Enter the text Analyze below" mode={mode}/>} />
-          <Route path="/about" element={<About/>} />
-  </Routes>
+  {/* <Routes> */}
+          {/* <Route path="/" element={<TextForm showAlert={showAlert} heading= "Enter the text Analyze below" mode={mode}/>} />   */}
+         <TextForm showAlert={showAlert} heading= "Enter the text Analyze below" mode={mode}/>
+          {/* <Route path="/about" element={<About/>} /> */}
+  {/* </Routes> */}
   </div>
-</Router>
+{/* </Router> */}
     </>
   );
 } 
